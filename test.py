1b3f5bbb39f5c1601c25f415253051d444dd24fc
VisweshBaskaran <viba5388@colorado.edu>
2023-12-11 19:03:20 -0700

    Python test code

---
+++ test.py
@@ -0,0 +1,16 @@
+# Replace ___ with your code
+
+# create variable number1 with value 9
+number1 = 9
+
+# create variable number2 with value 5
+number2 = 5
+
+# add number1 and number2
+result = number1 + number2
+result1 = number1 - number2
+result2 = number1 * number2
+# print result
+print(result)
+print(result1)
+print(result2)
